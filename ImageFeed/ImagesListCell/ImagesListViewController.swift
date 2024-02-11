fabf99dcbdd81ebf90ff13eb9c4dda0d66d24223
dreamlogin <dreamlogin@internet.ru>
2024-02-11 18:56:34 +0300

    layout with table in progress..

---
+++ ImageFeed/ImagesListCell/ImagesListViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by dreamlogin on 04.02.2024.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(ImagesListCell.self, forCellReuseIdentifier: ImagesListCell.reuseIdentifier)
+        tableView.rowHeight = 200
+    }
+
+    
+    func configCell(for cell: ImagesListCell) {
+    }
+
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            print("ImagesListViewController - EMPTY CELL SHOWN!")
+            return UITableViewCell()
+        }
+        configCell(for: imageListCell)
+        return imageListCell
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+
+
