7c2adbe47670ccbbaa39f88273f393c9bba1dbdb
dreamlogin <dreamlogin@vk.team>
2024-02-23 22:50:52 +0300

    "Single Image View" screen has been builded to view the selected image in full screen mode

--- ImageFeed/ SingleImageView/SingleImageViewController.swift
+++ ImageFeed/ SingleImageView/SingleImageViewController.swift
@@ -4,6 +4,11 @@ final class SingleImageViewController: UIViewController {
     var image: UIImage?
     @IBOutlet private var imageView: UIImageView!
 
+    @IBAction func backwardButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
