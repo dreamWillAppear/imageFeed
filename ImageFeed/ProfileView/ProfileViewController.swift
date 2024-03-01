a7c9b86309b7f56d5be6e844702f04feb9b41f2b
dreamlogin <dreamlogin@internet.ru>
2024-03-01 20:50:08 +0300

    rebuilt UI layout for the ProfileViewController screen using code layout

--- ImageFeed/ProfileView/ProfileViewController.swift
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -2,9 +2,97 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    private let profilePhoto = UIImageView()
+    private let username = UILabel()
+    private let nickname = UILabel()
+    private let profileDescription = UILabel()
+    private var logoutButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUIProfileViewController()
     }
     
+    private func setUIProfileViewController() {
+        setProfileImageStyle()
+        setProfileUsername()
+        setProfileNickname()
+        setProfileDescription()
+        setLogoutButton()
+        setConstraints()
+    }
+    
+    private func setProfileImageStyle() {
+        profilePhoto.image = UIImage(named: "User Mock Photo")
+        view.addSubview(profilePhoto)
+    }
+    
+    private func setProfileUsername() {
+        username.text = "Екатерина Новикова"
+        username.font = .boldSystemFont(ofSize: 23)
+        username.textColor = .ypWhite
+        view.addSubview(username)
+    }
+    
+    private func setProfileNickname() {
+        nickname.text = "@ekaterina_nov"
+        nickname.font = .systemFont(ofSize: 13)
+        nickname.textColor = .ypGray
+        view.addSubview(nickname)
+    }
+    
+    private func setProfileDescription() {
+        profileDescription.text = "Hello, world!"
+        profileDescription.font = .systemFont(ofSize: 13)
+        profileDescription.textColor = .ypWhite
+        view.addSubview(profileDescription)
+        
+    }
+    
+    private func setLogoutButton() {
+        logoutButton = UIButton.systemButton(
+            with: UIImage(systemName: "ipad.and.arrow.forward") ?? .stub,
+            target: self,
+            action: #selector(self.didTapLogoutButton))
+        
+        logoutButton.tintColor = .ypRed
+        view.addSubview(logoutButton)
+    }
+    
+    private func setConstraints() {
+        profilePhoto.translatesAutoresizingMaskIntoConstraints = false
+        username.translatesAutoresizingMaskIntoConstraints = false
+        nickname.translatesAutoresizingMaskIntoConstraints = false
+        profileDescription.translatesAutoresizingMaskIntoConstraints = false
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            //profilePhoto
+            profilePhoto.widthAnchor.constraint(equalToConstant: 70),
+            profilePhoto.heightAnchor.constraint(equalToConstant: 70),
+            profilePhoto.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            profilePhoto.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            
+            //username
+            username.topAnchor.constraint(equalTo: profilePhoto.bottomAnchor, constant: 8),
+            username.leadingAnchor.constraint(equalTo: profilePhoto.leadingAnchor),
+            
+            //nickname
+            nickname.topAnchor.constraint(equalTo: username.bottomAnchor, constant: 8),
+            nickname.leadingAnchor.constraint(equalTo: username.leadingAnchor),
+            
+            //profileDescription
+            profileDescription.topAnchor.constraint(equalTo: nickname.bottomAnchor, constant: 8),
+            profileDescription.leadingAnchor.constraint(equalTo: nickname.leadingAnchor),
+            
+            //logoutButton
+            logoutButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 55),
+            logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24)
+        ])
+    }
+    
+    @objc
+    private func didTapLogoutButton() {
+        print("ProfileViewController: - Did tap Logout Button!")
+    }
 }
