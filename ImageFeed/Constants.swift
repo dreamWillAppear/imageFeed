9a18825a1874e496fb3c9f7e7470ab442e025506
dreamlogin <dreamlogin@internet.ru>
2024-03-03 21:46:41 +0300

    added constants with access keys

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+enum Constants {
+    static let accessKey = "PD-M9E9ABNCwF4kD5bzVmNNIO9bWtz5gf8z7KOzUv2o"
+    static let secretKet = "g-fVrGUGbP7hFD-FXBntbOaI1oVVPOQHDCZ26KkHvT4"
+    static let reirecrtURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user_access+write_likes_access"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com/")
+}
+
+
